2b9f0a5b2079b3d4c63378f54415973696b18535
martenweijer <martenweijer@gmail.com>
2021-01-16 13:37:35 +0100

    Multiple template directories (#8)

--- tests/Loader/FilesystemLoaderTest.php
+++ tests/Loader/FilesystemLoaderTest.php
@@ -11,6 +11,13 @@ class FilesystemLoaderTest extends TestCase
         $this->assertEquals('Hello @name!', $loader->getContents('filesystemloadertest.template'));
 
         $this->expectException(RuntimeException::class);
-        $loader->getContents('foo');
+        $loader->getContents('bar');
+    }
+
+    public function testAddNamespace(): void
+    {
+        $loader = new FilesystemLoader(__DIR__);
+        $loader->addNamespace('foo', __DIR__ .'/foo');
+        $this->assertEquals('bar', $loader->getContents('foo::bar.html'));
     }
 }
